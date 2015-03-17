f1a8e005b73353e52ec73ead4a90559426aae5ae
Ben Johnson <ben@bixelcog.com>
2015-03-17 11:19:11 -0400

    Initial Commit

---
+++ Streaming Audio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Streaming Audio
+//
+//  Created by Ben Johnson on 3/17/15.
+//  Copyright (c) 2015 Bixelcog LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
